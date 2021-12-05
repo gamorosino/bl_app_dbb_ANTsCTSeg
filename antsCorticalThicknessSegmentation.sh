5a700cf41bcb8c024e67846671b94871ea3e699d
gamorosino <gamorosino@gmail.com>
2021-12-05 01:58:04 +0100

    minor change

--- antsCorticalThicknessSegmentation.sh
+++ antsCorticalThicknessSegmentation.sh
@@ -10,9 +10,7 @@ echo t1: ${1}
 echo template: ${2}
 echo threads ${3}
 echo output dir: ${4}
-echo brain mask: ${5} ;
-[ -z ${brain mask} ] || {  bmask_cmd="-z "${brain_mask}; }
-echo ${bmask_cmd}
+[ -z ${brain_mask} ] || {  bmask_cmd="-z "${brain_mask}; echo brain mask: ${5} ; }
 mkdir -p ${segment_dir}
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS
 #export ANTSPATH=/usr/lib/ants/
