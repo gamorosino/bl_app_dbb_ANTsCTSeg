87524c08959a5274ffe6d86b2bc15db159a593e8
gamorosino <gamorosino@gmail.com>
2022-04-23 10:07:39 +0200

    update of the dataset release

--- download_testset.sh
+++ download_testset.sh
@@ -5,5 +5,5 @@ output_dir=$1
 mkdir -p ${output_dir}
 for tag in ${tag_list[@]}; do
 	mkdir -p ${output_dir}'/'${tag}
-	bl bids download --pub "61b8b6b57ebe1c6fd7f048f2" --tag EMEDEA-PED --tag ${tag} --output ${output_dir}'/'${tag}
+	bl bids download --pub "6263ac49f3858674a29a89b8" --tag EMEDEA-PED --tag ${tag} --output ${output_dir}'/'${tag}
 done
